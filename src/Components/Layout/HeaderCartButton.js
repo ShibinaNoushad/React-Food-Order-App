31caa5ba32d196f33bd84e4e92a357538468c740
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-12-27 19:19:52 +0530

    Make number of items in cart dynamic

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,14 +1,24 @@
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
+import React, { useContext } from "react";
+
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../Store/CartContext";
 
 const HeaderCartButton = (props) => {
+  console.log("header button");
+  let quantity = 0;
+  const cartCntxt = useContext(CartContext);
+  cartCntxt.items.forEach((item) => {
+    quantity = quantity + Number(item.quantity);
+  });
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span>{cartCntxt.message}</span>
+      <span className={classes.badge}>{quantity}</span>
     </button>
   );
 };
