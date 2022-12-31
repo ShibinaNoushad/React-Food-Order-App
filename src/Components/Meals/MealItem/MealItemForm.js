5c771c2e279d8d512e858c3ab0e51758ee41bdba
ShibinaNoushad <shibinanoushad2224@gmail.com>
2022-12-31 14:47:21 +0530

    useref for collecting data from input

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
@@ -6,18 +6,22 @@ import CartContext from "../../../Store/CartContext";
 const MealItemForm = (props) => {
   const cartcntx = useContext(CartContext);
   console.log("meal item form initializd 1", cartcntx);
+  const amountInputRef = useRef();
 
   const addItemToCart = (e) => {
     e.preventDefault();
 
-    const quantity = document.getElementById("amount_" + props.id).value;
-    cartcntx.addItem({ ...props.item, quantity: quantity });
+    // const quantity = document.getElementById("amount_" + props.id).value;
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    cartcntx.addItem({ ...props.item, quantity: enteredAmountNumber });
   };
 
   return (
     <form className={classes.form}>
       {console.log("inside render", cartcntx.items)}
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
